8cde4e9fe9a9ee476abefc88fb2afe0c8300f032
Manuel Pastrana <72387430+Pastrana9@users.noreply.github.com>
2025-01-31 11:07:01 +0100

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,84 +1,35 @@
-import { MongoClient, ObjectId } from "mongodb"
-import { ContactModel } from "./type.ts";
-import { changeContact, getPhone } from "./utils.ts";
+import { MongoClient } from 'mongodb';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from "./typeDefs.ts";
+import { resolvers } from "./resolvers.ts";
+import { BookModel, LoansModel, UserModel } from "./type.ts";
 
-const MONGO_URL = Deno.env.get("MONGO_URL")
-
-if(!MONGO_URL) throw new Error("Error con MONGO_URL")
-
-const client = new MongoClient(MONGO_URL)
-await client.connect()
-console.log("Conectado a MongoDB")
-
-const db = client.db("agenda")
-const ContactCollection = db.collection<ContactModel>("contact")
-
-const handler = async(req:Request):Promise<Response> => {
-  const url = new URL(req.url)
-  const method = req.method
-  const path = url.pathname
-
-  if(method === "GET") {
-    if(path === "/contact") {
-      const result = await ContactCollection.find().toArray()
-      const resultFinal = await Promise.all(result.map(e => changeContact(e)))
-      return new Response(JSON.stringify(resultFinal))
-    }
-  } else if(method === "POST") {
-    if(path === "/contact") {
-      const body = await req.json()
-      if(!body.name || !body.phone) return new Response("Bad request", {status:404})
-      const comprobarPhone = await ContactCollection.findOne({phone: body.phone})
-      if(comprobarPhone) return new Response("User already exists", {status:404})
-      const { is_valid ,country, timezone } = await getPhone(body.phone)
-      if(!is_valid) return new Response("Telefono no valido", {status:404})
-      const args = { ...body, country, timezone }
-      const { insertedId } = await ContactCollection.insertOne({...args})
-      return new Response(JSON.stringify({
-        id: insertedId.toString(),
-        ...args
-      }))
-    }
-  } else if(method === "PUT") { 
-    if(path === "/contact") {
-      const body = await req.json()
-      if(!body.id) return new Response("Bad request", {status:404})
-      if(body.phone) {
-        const comprobarPhone = await ContactCollection.findOne({phone: body.phone})
-        if(comprobarPhone) return new Response("User already exists", {status:404})
-        const { is_valid, country, timezone } = await getPhone(body.phone)
-        if(!is_valid) return new Response("Telefono no valido", {status:404})
-        const { id, ...update} = body
-        const args = { ...update, country, timezone }
-        const result = await ContactCollection.findOneAndUpdate(
-          {_id: new ObjectId(id)},
-          {$set: {...args}},
-          {returnDocument: "after"}
-        )
-        if(!result) return new Response("User not exist", {status:400})
-        return new Response(JSON.stringify(await changeContact(result)))
-      } else {
-        const result = await ContactCollection.findOneAndUpdate(
-          {_id: new ObjectId(body.id)},
-          {$set: {name: body.name}},
-          {returnDocument: "after"}
-        )
-        if(!result) return new Response("User not exist", {status:400})
-        return new Response(JSON.stringify(await changeContact(result)))
-      }
-
-      
-    }
-  } else if(method === "DELETE") {
-    if(path === "/contact") {
-      const body = await req.json()
-      if(!body.id) return new Response("Bad request", {status:404})
-      const { deletedCount } = await ContactCollection.deleteOne({_id: new ObjectId(body.id)})
-      if(deletedCount === 0) return new Response(JSON.stringify(false))
-      return new Response(JSON.stringify(true))
-    }
-  }
-  return new Response("Endpoint not found", {status:400})
+const MONGO_URL = Deno.env.get("MONGO_URL") || "";
+if (!MONGO_URL) {
+  console.error("MONGO_URL no está definido. Verifica tus variables de entorno.");
+  Deno.exit(1);
 }
 
-Deno.serve({port:4000},handler)
+let client;
+try {
+  client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log("Conectado a MongoDB");
+} catch (error) {
+  console.error("Error conectando a MongoDB:", error);
+  Deno.exit(1);
+}
+
+const db = client.db("biblioteca");
+const UserCollection = db.collection<UserModel>("user");
+const BookCollection = db.collection<BookModel>("book");
+const LoansCollection = db.collection<LoansModel>("loan");
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
+  context: async () => ({ UserCollection, BookCollection, LoansCollection }),
+});
+
+console.log(`🚀 Servidor listo en: ${url}`);
